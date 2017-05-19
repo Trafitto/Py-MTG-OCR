d7504f3974d4e8170433d96bc9cb45145b9a8e43
Marco Borchi <borchimarco@gmail.com>
2017-05-19 11:20:38 +0200

    Fixing search loop and add test with some photo

--- PySeek&Scan.py
+++ PySeek&Scan.py
@@ -8,11 +8,15 @@ from mtgsdk import Subtype
 from mtgsdk import Changelog
 import enchant
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+
+#print (enchant.dict_exists("en_US"))		 #Dict chek
+#print(enchant.list_languages())
+
 #print(pytesseract.image_to_string(Image.open('C:\\Users\\Marco Borchi\\Desktop\\242.jpg'),lang='ita'))
 #print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='ita'))
-im = Image.open("goblinENG.jpg") #NOTE: WITH 500X500 IMG NO PROBLEM TO READ THE TEXT
+im = Image.open("Pianura2.jpg") #NOTE: WITH 500X500 IMG NO PROBLEM TO READ THE TEXT
 text=''
-enchanceIndex=1
+enchanceIndex=1 #per fare una prova ho visto che da 12 in poi legge bene
 w,h=im.size
 im.crop((0,0,w,h-250)).save("temp.jpg")
 im2=Image.open("temp.jpg")#.convert('L')
@@ -20,32 +24,57 @@ im2=Image.open("temp.jpg")#.convert('L')
 ReadedText=[]
 enhancer = ImageEnhance.Contrast(im2)
 im2 = im2.filter(ImageFilter.MinFilter(3))
-while enchanceIndex<=20: #Testing
+d = enchant.DictWithPWL("en_US","MagicCardName.txt")
+while enchanceIndex<=15: #Testing
 	
 	im2 = enhancer.enhance(enchanceIndex)
 	im2 = im2.convert('1')
-	im2.show()
+	#im2.show()
 	text =(pytesseract.image_to_string(im2,lang='ita'))
 	 #print (text)
-	print(enchanceIndex)
+	print('\nValore contrasto= ',enchanceIndex)
 	enchanceIndex+=1
 	if text!='' :
-		ReadedText.append(text)
-d = enchant.request_dict("en_US")
-	
+		#ReadedText.append(text)
+		print ('\n---------Name of Cards---------\n')
+		print ('Testo rilevato ',text)
+		print ('Testi suggeriti ',d.suggest(text))
+		suggerimenti=d.suggest(text)
+		if (len(suggerimenti)>0):
+			print('Ricerca...')
+			for s in suggerimenti:
+				if s==text:
+					cardToSearch=s
+				else:
+					cardToSearch=suggerimenti[0] #quella con maggior probabilità di essere esatta
+			
+			print ('Cerca -> ',cardToSearch)
+			cards=Card.where(name=cardToSearch).all()
+			if (len(cards)>0):
+				for c in cards:
+					print(c.name,' ',c.cmc ,c.colors)
+				break
+			else:
+				cardsITA=Card.where(language="Italian").where(name=cardToSearch).all()
+				if (len(cardsITA)>0):
+					for c in cardsITA:
+						print(c.name,' ',' costo= ',c.cmc ,' colore= ', c.colors)
+					break
 #Cerca nel dizionario	
-for text in ReadedText:
-	print (text)
+#for text in ReadedText:
 	
-	for t in text.split(' '):
-		print (d.suggest(t))
+	
+	#print ('\n---------Splittati---------\n')
+	#for t in text.split(' '):
+		#print (t)
+		#print (d.suggest(t))
 	
 
 #Cerca con le api di magic
 #nomes=text.split(' ')
-nomes=ReadedText
-for n in nomes:
-	cards=Card.where(language="Italian").where(name=n).all()
+#nomes=ReadedText
+#for n in nomes:
+	#cards=Card.where(language="Italian").where(name=n).all()
 
-	for c in cards:
-		print(c.name,' ',c.cmc ,c.colors)	
+	#for c in cards:
+		#print(c.name,' ',c.cmc ,c.colors)	
